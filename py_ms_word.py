02fba2665cdc7606ad0b29aed645fe5cb21daffa
Caner Erden <cerden@sakarya.edu.tr>
2018-12-09 14:14:07 +0300

    py_word

---
+++ py_ms_word.py
@@ -0,0 +1,44 @@
+"""
+author: Caner Erden
+"""
+try:
+    from xml.etree.cElementTree import XML
+except ImportError:
+    from xml.etree.ElementTree import XML
+import zipfile
+
+WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+PARA = WORD_NAMESPACE + 'p'
+TEXT = WORD_NAMESPACE + 't'
+
+
+def get_docx_text(path='C:\\Users\\cerde\Desktop\\py_word'):
+    """
+    Take the path of a docx file as argument, return the text in unicode.
+    """
+    document = zipfile.ZipFile(path)
+    contentToRead = ["header2.xml", "document.xml", "footer2.xml"]
+    paragraphs = []
+
+    for xmlfile in contentToRead:
+        xml_content = document.read('word/{}'.format(xmlfile))
+        tree = XML(xml_content)
+        for paragraph in tree.getiterator(PARA):
+            texts = [node.text
+                     for node in paragraph.getiterator(TEXT)
+                     if node.text]
+            if texts:
+                textData = ''.join(texts)
+                if xmlfile == "footer2.xml":
+                    extractedTxt = "Footer : " + textData
+                elif xmlfile == "header2.xml":
+                    extractedTxt = "Header : " + textData
+                else:
+                    extractedTxt = textData
+
+                paragraphs.append(extractedTxt)
+    document.close()
+    return '\n\n'.join(paragraphs)
+
+
+print(get_docx_text())
