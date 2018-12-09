363fe76333d46762f9ff287bbe1838bf400dc426
Caner Erden <cerden@sakarya.edu.tr>
2018-12-09 14:01:39 +0300

    excel_link.py oluşturuldu

---
+++ open_links_from_excel.py
@@ -0,0 +1,15 @@
+import xlrd, webbrowser
+
+#put this script to the same file with websites.xlsx which contains the links
+
+workbook = xlrd.open_workbook('websites.xlsx')
+# Sheet name
+sheet = workbook.sheet_by_name('Sayfa1')
+
+# Suppose your URLs are in column 5, rows 2 to 30
+url_column = 5
+
+# This will open 28 webpages on your browser
+for row in range(2, 30):
+    url = sheet.cell_value(row, url_column)
+    webbrowser.open_new_tab(url)
