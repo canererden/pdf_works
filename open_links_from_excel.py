b7f811ccd442a96cdc3333b078bea35c3b59df2a
Caner Erden <33541769+canererden@users.noreply.github.com>
2018-12-09 15:28:00 +0300

    Update open_links_from_excel.py

--- open_links_from_excel.py
+++ open_links_from_excel.py
@@ -9,10 +9,10 @@ workbook = xlrd.open_workbook('websites.xlsx')
 # Sheet name
 sheet = workbook.sheet_by_name('Sayfa1')
 
-# Suppose your URLs are in column 5 which is F column, rows 2 to 6 which are F2, F3, F4, F5 cells
+# Suppose your URLs are in column 5 which is F column, rows 2 to 6 which are F2, F3, F4, F5, F6 cells
 url_column = 5
 
-# This will open 28 webpages on your browser
+# This will open 5 webpages on your browser
 for row in range(1, 6):
     url = sheet.cell_value(row, url_column)
     webbrowser.open_new_tab(url)
